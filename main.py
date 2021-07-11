1b722a63c41960771fb1ced547e4de00b6252596
Michel de Araujo <78417934+micheldearaujo@users.noreply.github.com>
2021-07-11 17:11:27 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+This is a Data Science project. The Data-flair website call this project 'Advanced', so, let's see!
+
+https://data-flair.training/blogs/advanced-python-project-detecting-fake-news/
+
+created on 2021 July 10 by @micheldearaujo
+
+"""
+
